d50a51291dbd9fd716ccb81ac7333427d939baa8
a07757 <alvinhosemedo19@gmail.com>
2018-02-01 16:57:18 -0100

    add java-vendas-jdenner

---
+++ java-sistema-vendas-master/src/com/jdenner/gui/Sobre.java
@@ -0,0 +1,58 @@
+package com.jdenner.gui;
+
+/**
+ * Janela sobre a aplicação
+ *
+ * @author Juliano
+ */
+public class Sobre extends javax.swing.JDialog {
+
+    public Sobre(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        lbIcone = new javax.swing.JLabel();
+        taTexto = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Sobre");
+        setResizable(false);
+        getContentPane().setLayout(new java.awt.GridBagLayout());
+
+        lbIcone.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/jdenner/gui/img/sobre.png"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(0, 20, 0, 20);
+        getContentPane().add(lbIcone, gridBagConstraints);
+
+        taTexto.setEditable(false);
+        taTexto.setColumns(20);
+        taTexto.setFont(new java.awt.Font("SansSerif", 0, 11)); // NOI18N
+        taTexto.setRows(5);
+        taTexto.setText("Sistema de Vendas\nAutor: ´Álvaro Junior Semedo Soares\nE-mail: alvinhosemedo19@gmail.com\n\nAplicação desenvolvida como material\nde apoio ao aprendizado da desciplina\nde desenvolvimento de sistemas de \ninfomação"); // NOI18N
+        taTexto.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        taTexto.setOpaque(false);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(taTexto, gridBagConstraints);
+        getContentPane().add(jLabel1, new java.awt.GridBagConstraints());
+
+        setSize(new java.awt.Dimension(395, 222));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lbIcone;
+    private javax.swing.JTextArea taTexto;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ java-sistema-vendas-master/src/com/jdenner/to/Cliente.java
@@ -0,0 +1,93 @@
+package com.jdenner.to;
+
+import java.util.Date;
+
+/**
+ * Classe contendo os dados do cliente
+ *
+ * @author Juliano
+ */
+public class Cliente {
+
+    /**
+     * @return the telefone
+     */
+    public int getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    private int codigo;
+    private String nome;
+    private String cpf;
+    private int telefone;
+    private Date dataNascimento;
+
+    public Cliente() {
+        this.codigo = 0;
+        this.nome = "";
+        this.cpf = "";
+        this.dataNascimento = new Date();
+    }
+
+    public Cliente(int codigo) {
+        this.codigo = codigo;
+        this.nome = "";
+        this.cpf = "";
+        this.dataNascimento = new Date();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return getNome();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Cliente) {
+            Cliente c = (Cliente) o;
+            if (c.getCodigo() == this.getCodigo()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
